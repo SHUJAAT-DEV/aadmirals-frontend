a92d5b657c411b66afffe41a7d68eb4cbe34944c
salik-js <balisaalik@gmail.com.com>
2022-12-29 17:47:49 +0500

    Aadmirals issues fixing about design

--- pages/houston-airport-transportation/index.jsx
+++ pages/houston-airport-transportation/index.jsx
@@ -20,12 +20,12 @@ import Head from "next/head";
 import * as api from "../../api";
 import axios from "axios"
 import { URL } from "../../config/serverUrl";
-import ReactHtmlParser from 'react-html-parser'; 
+import ReactHtmlParser from 'react-html-parser';
 import Link from 'next/link';
 import { useRouter } from 'next/router'
 import Image from 'next/image'
 
-import Testinmonial from '../../Components/Testimonial/Testimonial' 
+import Testinmonial from '../../Components/Testimonial/Testimonial'
 
 
 function AirportTransportationHouston(props) {
@@ -52,18 +52,18 @@ function AirportTransportationHouston(props) {
   const data = houstonAirportTransportation && houstonAirportTransportation.houstonAirportTransportation[0]
   const faqs = houstonAirportTransportation && houstonAirportTransportation.faqs
   const fleet = houstonAirportTransportation && houstonAirportTransportation.fleet
-//   const cityWeServe = houstonAirportTransportation && houstonAirportTransportation.cityWeServe
+  //   const cityWeServe = houstonAirportTransportation && houstonAirportTransportation.cityWeServe
   const testimonial = houstonAirportTransportation && houstonAirportTransportation.testimonial
-// 
+  // 
 
-const rawMarkup=(data) =>{
-  var rawMarkup = data;
-  return { __html: rawMarkup };
-}
+  const rawMarkup = (data) => {
+    var rawMarkup = data;
+    return { __html: rawMarkup };
+  }
   return (
     <>
       <Head>
-    <link rel="canonical" href={`https://aadmirals.com${router.pathname}`} />
+        <link rel="canonical" href={`https://aadmirals.com${router.pathname}`} />
         <meta charSet="utf-8" />
         <title>{data?.metaTitle}</title>
         <meta
@@ -76,27 +76,27 @@ const rawMarkup=(data) =>{
       <Header />
 
       <div className={styles.mainContainer} fluid id="#bookingForm">
-      <Hero
-           Text={data.ShortDescriptionBanner} Title={'Houston Airport Transportation & Shuttle Service'}  img={data.bannerImage}  Form={HomeForm}
+        <Hero
+          Text={data.ShortDescriptionBanner} Title={'Houston Airport Transportation & Shuttle Service'} img={data.bannerImage} Form={HomeForm}
         />
       </div>
 
 
-  
+
       <div className="container mt-3 mb-3">
-        { houstonAirportTransportation.houstonAirportTransportation.map((data,key) => {
+        {houstonAirportTransportation.houstonAirportTransportation.map((data, key) => {
           return (
-            <div className="row"key={key}>
+            <div className="row" key={key}>
               <div className="col-12">
-             { <p>{ ReactHtmlParser (data.DescriptionAfterBanner ) }</p>} 
-              {/* <span>{data.metaTitle}</span> */}
-                 <Image priority={true} src={data.ImageAfterBanner1st} className='w-100 mb-4 mt-4' alt="Airport Transportation Houston" width={1200} layout="responsive"  height={500} quality={100}/>
-                { <p>{ ReactHtmlParser (data.DescriptionAfterBannerafterImage) }</p>} 
-                 <Image priority={true} src={data.ImageAfterBanner2nd} className='w-100 mb-4 mt-4' alt="AAdmirals Airport Transportation Houston" width={1200} layout="responsive"  height={400} quality={100} />
-                { <p>{ ReactHtmlParser (data.DescriptionAfterAannerAfterImage2nd) }</p>}                        
-         
-                 <Image priority={true} src={data.ImageAfterBanner3rd} className='w-100 mb-4 mt-4' alt="663" width={1200} layout="responsive"  height={400} quality={100}/>
-                { <p>{ ReactHtmlParser (data.DescriptionAfterBannerAfterImage3rd ) }</p>}                        
+                {<p>{ReactHtmlParser(data.DescriptionAfterBanner)}</p>}
+                {/* <span>{data.metaTitle}</span> */}
+                <Image priority={true} src={data.ImageAfterBanner1st} className='w-100 mb-4 mt-4' alt="Airport Transportation Houston" width={1200} layout="responsive" height={500} quality={100} />
+                {<p>{ReactHtmlParser(data.DescriptionAfterBannerafterImage)}</p>}
+                <Image priority={true} src={data.ImageAfterBanner2nd} className='w-100 mb-4 mt-4' alt="AAdmirals Airport Transportation Houston" width={1200} layout="responsive" height={400} quality={100} />
+                {<p>{ReactHtmlParser(data.DescriptionAfterAannerAfterImage2nd)}</p>}
+
+                <Image priority={true} src={data.ImageAfterBanner3rd} className='w-100 mb-4 mt-4' alt="663" width={1200} layout="responsive" height={400} quality={100} />
+                {<p>{ReactHtmlParser(data.DescriptionAfterBannerAfterImage3rd)}</p>}
                 {/* <img src={data.ImageAfterBanner4th} className='w-100 mb-4 mt-4' alt="664" />
                 { <p>{ ReactHtmlParser ( data.DescriptionAfterBannerAfterImage4th  ) }</p>}                         */}
               </div>
@@ -105,36 +105,36 @@ const rawMarkup=(data) =>{
         })}
       </div>
       <div className="container services-links mb-4">
-          <h3>Services</h3>
-          <div>
-            {
-              data.dropdown.map((data1,key)=>{
-                return(
-                  <li key={key}><Link href={data1.url}><a>{data1.name}</a></Link></li>
-                )
-              })
-            }
-          </div>
+        <h3>Services</h3>
+        <div>
+          {
+            data.dropdown.map((data1, key) => {
+              return (
+                <li key={key}><Link href={data1.url}><a>{data1.name}</a></Link></li>
+              )
+            })
+          }
         </div>
+      </div>
       <div className="container">
         <div className="row">
-          {fleet.map((data,key)=>{
-            return(
+          {fleet.map((data, key) => {
+            return (
               <div className="col-3 carCardContainer" key={key}>
                 <div className="imageCardContainer">
-                 <Image priority={true} src={data.image} className='w-100 rounded bordered' alt={data?.name} quality={100}   width={500} height={600} objectFit="cover" />
+                  <Image priority={true} src={data.image} className='w-100 rounded bordered' alt={data?.name} quality={100} width={500} height={600} objectFit="cover" />
                 </div>
                 <h6>{data.name}</h6>
                 <p>{data.description && data.description.substring(0, 20)}</p>
               </div>
             )
           })}
-         
+
         </div>
-      </div> 
-      
-      <Testinmonial testimonials={testimonial}/>
-      
+      </div>
+
+      <Testinmonial testimonials={testimonial} />
+
 
       <Askquestion faqs={faqs} />
 
@@ -142,15 +142,15 @@ const rawMarkup=(data) =>{
 
 
 
-<br></br>
-<br></br>
-<br></br>
+      <br></br>
+      <br></br>
+      <br></br>
       <Footer />
 
     </>
   );
 }
-export async function getStaticProps ({ query }) {
+export async function getStaticProps({ query }) {
   let res2 = await axios.get(`${URL}/website-content/airport-transportation-houston`);
   // 
   let data2 = res2.data.modifiedResponse
