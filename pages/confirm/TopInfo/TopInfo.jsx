ae5d13875a88d8be0f279ffb46b14dfd4f1d01f9
salik-js <balisaalik@gmail.com.com>
2023-01-07 17:11:59 +0500

    confirm component breadcumbs adjust

--- pages/confirm/TopInfo/TopInfo.jsx
+++ pages/confirm/TopInfo/TopInfo.jsx
@@ -18,30 +18,80 @@ import {
 } from "reactstrap";
 
 function TopInfo({ quotes }) {
+  console.log("Quotes checking", quotes);
   return (
-    <Container fluid className={styles.data}>
-      {quotes ? (
-        <h6>
-          {quotes.otherDetails.from} 
-          {" "}
-          {quotes.otherDetails.to}{" "}
-          {" "}
-          {`Duration: ${
-            quotes.otherDetails.duration
-              ? quotes.otherDetails.duration
-              : `${quotes.otherDetails.hours} hours`
-          } – Distance: ${
-            quotes.otherDetails.distance ? quotes.otherDetails.distance : "N/A"
-          }`}
-          {" "}
-          {quotes.otherDetails.date}
-          {" "}
-          {" "}
-          {quotes.otherDetails.time}
-        </h6>
-      ) : (
-        "Loading"
-      )}
+    <Container fluid
+      className={styles.data}
+      style={{
+      }}
+    >
+      <div style={{
+        alignContent: "center"
+      }}>
+        {quotes ? (
+          <h6
+            style={{
+              textAlign: "center"
+            }}
+          >
+            <span
+              style={{
+                padding: "5px 5px",
+                borderRadius: "10px",
+                border: "1px solid"
+              }}
+            >
+              {quotes.otherDetails.from}
+            </span>
+            {" "}
+            <span
+              style={{
+                padding: "5px 5px",
+                borderRadius: "10px",
+                border: "1px solid",
+                marginRight: "4px"
+              }}
+            >
+              {quotes.otherDetails.to}{" "}
+            </span>
+            <span
+              style={{
+                padding: "5px 5px",
+                borderRadius: "10px",
+                border: "1px solid"
+              }}
+            >
+              {`Duration: ${quotes.otherDetails.duration
+                ? quotes.otherDetails.duration
+                : `${quotes.otherDetails.hours} hours`
+                } – Distance: ${quotes.otherDetails.distance ? quotes.otherDetails.distance : "N/A"
+                }`}
+            </span>
+      
+            {" "}
+            <span
+              style={{
+                padding: "5px 5px",
+                borderRadius: "10px",
+                border: "1px solid",
+                
+              }}>
+              {quotes.otherDetails.date}
+            </span>
+            {" "}
+            <span
+              style={{
+                padding: "5px 5px",
+                borderRadius: "10px",
+                border: "1px solid"
+              }}>
+              {quotes.otherDetails.time}
+            </span>
+          </h6>
+        ) : (
+          "Loading"
+        )}
+      </div>
     </Container>
   );
 }
