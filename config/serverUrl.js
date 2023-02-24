20f572067f43c8a6fbb8780f08e7fa6cce34463a
shujaat <shujaat.dev@taazaa.com>
2023-02-24 22:56:47 +0500

    remove the proxy and added the new base url

--- config/serverUrl.js
+++ config/serverUrl.js
@@ -1,4 +1,4 @@
 // export const URL = "https://admirals.herokuapp.com";
-// export const URL = "https://admin.aadmirals.com"
-export const URL = "http://aadmiralapi-env.eba-2bmqy7kt.us-east-2.elasticbeanstalk.com"
+export const URL = "https://api.aadmirals.com"
+// export const URL = https://api.aadmirals.com
 // export const URL = "http://localhost:5000"
