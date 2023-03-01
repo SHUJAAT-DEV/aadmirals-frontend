b21d2fcd348ca70999d8b9ae5ee3a38f96cdd79b
salik-dev <bahadurali0346556@gmail.com>
2023-03-01 14:28:28 +0500

    destination cards section updated

--- Components/front-end/destinationCardsSection1.jsx
+++ Components/front-end/destinationCardsSection1.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 // import "./style.scss"
 
-const CardsSection1 = () => { 
-    return (    
+const CardsSection1 = () => {
+    return (
         <div className="cards1">
             <h4>George Bush Airport</h4>
             <p>Downtown Houston </p>
             <p>Approx. 24 mile</p>
-            <button>BOOK NOW</button>
+            <button>Book Now
+            <span className="right-arrow"> &#8594;</span>
+            </button>
         </div>
     )
 }
