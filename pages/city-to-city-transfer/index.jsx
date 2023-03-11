02b64323b96e456047c0040aa5dc468a2c660237
saqlain-codecraf <mushtaqasm786@gmail.com>
2023-03-11 03:54:04 -0800

    add event and city pages

--- pages/city-to-city-transfer/index.jsx
+++ pages/city-to-city-transfer/index.jsx
@@ -1,204 +1,70 @@
 import React, {  useEffect } from "react";
-
+import { Container, Row, Col } from "reactstrap";
 import Header from "../../Components/Header/Header";
 import styles from "../home.module.scss";
-
-import Askquestion from "../../Components/askquestions/askquestion";
 import Footer from "../../Components/Footer/Footer";
-
 import Hero from "../../Components/hero/hero";
 import Floatingbutton from "../../Components/floaingbutton/floatingbutton";
-
 import SideNav from "../../Components/Header/SideNav/SideNav";
-
 import HomeForm from "../../Components/Home Form/HomeForm";
-
-import { useDispatch } from 'react-redux'
-
-import axios from "axios"
-import { URL } from "../../config/serverUrl";
-import Link from "next/link"
-import ReactHtmlParser from 'react-html-parser'; 
+import Loader from "../../Components/Loader/Loader";
 import { useRouter } from 'next/router'
-import Image from "next/image";
-import Testinmonial from '../../Components/Testimonial/Testimonial' 
+import Home from '../../Components/front-end/Home';
+import  getServiceContentById  from "../../utils/services/serviceApi";
+import useService from "../../utils/services/useServices";
+import { dehydrate, QueryClient } from '@tanstack/react-query';
 import {NextSeo} from 'next-seo'
+import limo from '../../Assets/download.jpg'
 
 function CityToCityTransfer(props) {
-  const router = useRouter()
-
-  const cms = props.data1
-
-  const { citytoCityTransfer } = cms
-
-  const data = citytoCityTransfer && citytoCityTransfer.citytoCityTransfer[0]
-  const faqs = citytoCityTransfer && citytoCityTransfer.faqs
-  const fleet = citytoCityTransfer && citytoCityTransfer.fleet
-
-  const testimonial = citytoCityTransfer && citytoCityTransfer.testimonial
-
-
-const rawMarkup=(data) =>{
-  var rawMarkup = data;
-  return { __html: rawMarkup };
-}
+  const router = useRouter();
+  const {serviceContent ,isLoading, isError,error}= useService("640c65af4719540d93c3d6da")
   return (
     <>
-    <NextSeo
-            title={data?.metaTitle}
-            description={data?.metaDescription}
-            canonical={`https://aadmirals.com${router?.pathname}`}
-           
-            
-        />
-     <div style={{ overflow: "hidden" }}>
-      <SideNav />
-      <Floatingbutton />
-      <Header />
-
-      <div className={styles.mainContainer} fluid id="#bookingForm">
-      <Hero
-           Text={""} Title={data.Description1}  img={data.Image1}  Form={HomeForm}
-        />
-      </div>
-
-
-  
-      <div className="container mt-3 mb-3 breakword_paragraph">
-      { citytoCityTransfer.citytoCityTransfer.map((data,key) => {
-          return (
-            <div className="row" key={key}>
-              <div className="col-12">
-              { <p>{ ReactHtmlParser (data.Description2  ) }</p>}  
-               <Image priority={true}
-                      width="100"
-                      src={data.Image2}
-                      alt="AAdmirals Houston Limo Service “City to “City”"
-                      height={40}
-                      className='w-100 mb-4 mt-4'
-                      layout='responsive'
-                      
-                      objectFit="cover"
-                      quality={100}
-                    />                       
-                     <Image priority={true}
-                      width="100"
-                      src={data.Image3}
-                      alt="AAdmirals Houston Limo Service “City to “City”"
-                      height={70}
-                      className='w-100 mb-4 mt-4'
-                      layout='responsive'
-                      
-                      objectFit="cover"
-                      quality={100}
-                    />   
-                    <br/>
-            
-            
-                { <p>{ ReactHtmlParser (data.Description3  ) }</p>}    
-                 <Image priority={true}
-                      width="100"
-                      src={data.Image4}
-                      alt="AAdmirals Houston Limo Service “City to “City”"
-                      height={40}
-                      className='w-100 mb-4 mt-4'
-                      layout='responsive'
-                      
-                      objectFit="cover"
-                      quality={100}
-                    />                      
-               <br/>       
-                { <p>{ ReactHtmlParser (data.Description4  ) }</p>}  
-                 <Image priority={true}
-                      width="100"
-                      src={data.Image5}
-                      alt="AAdmirals Houston Limo Service “City to “City”"
-                      height={70}
-                      className='w-100 mb-4 mt-4'
-                      layout='responsive'
-                      
-                      objectFit="cover"
-                      quality={100}
-                    />      
-                
-                { <p>{ ReactHtmlParser (data.Description5  ) }</p>}  
-                
-                 <Image priority={true}
-                      width="100"
-                      src={data.Image51}
-                      alt="AAdmirals Houston Limo Service “City to “City”"
-                      height={70}
-                      className='w-100 mb-4 mt-4'
-                      layout='responsive'
-                      
-                      objectFit="cover"
-                      quality={100}
-                    /> 
-                    <br/>
-                { <p>{ ReactHtmlParser (data.Description6  ) }</p>}  
-
-                       
-              </div>
-            </div>
-          )
-        })}
-      </div>
-      <div className="container services-links mb-4">
-          <h3>Services</h3>
-          <div>
-            {
-              data.dropdown.map((data,key)=>{
-                return(
-                  <li key={key}><Link href={data.url}><a>{data.name}</a></Link></li>
-                )
-              })
-            }
+      <NextSeo
+        title={serviceContent?.metaTitle}
+        description={serviceContent?.metaDescription}
+        canonical={`https://aadmirals.com${router?.pathname}`}
+      />
+      {isError || isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <div style={{ overflow: "hidden" }}>
+            <SideNav />
+            <Floatingbutton />
+            <Header />
+            {error ? (
+              <Alert className="m-0" color="danger">
+                {error}
+              </Alert>
+            ) : null}
+            <Container className={`${styles.mainContainer} p-0`} fluid>
+              <Hero
+                Text={serviceContent?.heroDescription}
+                Title={
+                  serviceContent?.heroTitle ||
+                  "Galveston Cruise Transfer & Shuttle | IAH/Hobby Airport to Glaveston"
+                }
+                img={serviceContent?.heroImage || limo }
+                Form={HomeForm}
+              />
+            </Container>
+            <Home serviceDetail={serviceContent} />
+            <Footer />
           </div>
-        </div>
-      <div className="container">
-        <div className="row">
-          {fleet.map((data,key)=>{
-            return(
-              <div className="col-md-3 carCardContainer" key={key}>
-                <div className="imageCardContainer">
-                 <Image priority={true} width={100} height={130} objectFit="cover" quality={100} src={data.image} alt={data.name}  layout='responsive' className='w-100 rounded bordered' />
-               
-                </div>
-                <h6>{data.name}</h6>
-                <p>{data.description && data.description.substring(0, 20)}</p>
-              </div>
-            )
-          })}
-         
-        </div>
-      </div> 
-      
-      <Testinmonial testimonials={testimonial}/>
-      
-
-      <Askquestion faqs={faqs} />
-
-
-
-
-
-<br></br>
-<br></br>
-<br></br>
-      <Footer />
-</div>
+        </>
+      )}
     </>
   );
 }
-export async function getStaticProps ({ query }) {
-  let res2 = await axios.get(`${URL}/website-content/city-to-city-transfer`);
-  // 
-  let data2 = res2.data.modifiedResponse
-  let data1 = {
-    loading: false,
-    error: null,
-    citytoCityTransfer: data2
+export async function getStaticProps({ query }) {
+  const queryClient = new QueryClient()
+  await queryClient.prefetchQuery(['service-page', "640c65af4719540d93c3d6da"], getServiceContentById("640c65af4719540d93c3d6da"))
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
   }
-  return { props: { data1 } }
 }
 export default CityToCityTransfer;
--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,160 +1,71 @@
-import React, {useState, useEffect} from "react";
-import {Container, Row, Col} from "reactstrap";
+import {Container} from "reactstrap";
 import Header from "../../Components/Header/Header";
 import styles from "../home.module.scss";
-import Fleet from "../../Components/fleet/fleet";
-import Askquestion from "../../Components/askquestions/askquestion";
 import Footer from "../../Components/Footer/Footer";
-import Cities from "../../Components/Cities/Cities";
 import Hero from "../../Components/hero/hero";
 import Floatingbutton from "../../Components/floaingbutton/floatingbutton";
-import Testimonial from "../../Components/Testimonial/Testimonial";
 import SideNav from "../../Components/Header/SideNav/SideNav";
-import logo from "../../Assets/Group 943.png";
 import HomeForm from "../../Components/Home Form/HomeForm";
 import {Alert} from "reactstrap";
 import Loader from "../../Components/Loader/Loader";
-import {useDispatch, useSelector} from "react-redux";
-import {getGalvestonCruisePage} from "../../redux/Services/Galveston_cruise_transportation/action";
-import Head from "next/head";
-import * as api from "../../api";
-import axios from "axios";
-import {URL} from "../../config/serverUrl";
-import Link from "next/link";
-import ReactHtmlParser from "react-html-parser";
 import {useRouter} from "next/router";
 
 import {NextSeo} from "next-seo";
+import Home from '../../Components/front-end/Home';
+import  getServiceContentById  from "../../utils/services/serviceApi";
+import useService from "../../utils/services/useServices";
+import { dehydrate, QueryClient } from '@tanstack/react-query';
+import bg from '../../Assets/download.jpg'
 
-import Testinmonial from "../../Components/Testimonial/Testimonial";
-
-function Events(props) {
+function Events() {
   const router = useRouter();
-
-  const dispatch = useDispatch();
-
-  // const cms = useSelector(state => state.galvestonCruise)
-  // const { galveston_cruise_page } = cms
-
-  // const data = galveston_cruise_page && galveston_cruise_page.galvestonCruiseTransfer[0]
-  // const faqs = galveston_cruise_page && galveston_cruise_page.faqs
-  // const fleet = galveston_cruise_page && galveston_cruise_page.fleet
-  // const cityWeServe = galveston_cruise_page && galveston_cruise_page.cityWeServe
-  // const testimonial = galveston_cruise_page && galveston_cruise_page.testimonial
-
-  useEffect(() => {
-    // dispatch(getGalvestonCruisePage())
-  }, []);
-  const cms = props.data1;
-  //
-  const {eventsTransfer} = cms;
-
-  const data = eventsTransfer && eventsTransfer.eventsTransfer[0];
-  const faqs = eventsTransfer && eventsTransfer.faqs;
-  const fleet = eventsTransfer && eventsTransfer.fleet;
-  //   const cityWeServe = houstonAirportTransportation && houstonAirportTransportation.cityWeServe
-  const testimonial = eventsTransfer && eventsTransfer.testimonial;
-  //
-
-  const rawMarkup = (data) => {
-    var rawMarkup = data;
-    return {__html: rawMarkup};
-  };
+  const {serviceContent ,isLoading, isError,error}= useService("640c6a8e4719540d93c3d76d")
   return (
     <>
       <NextSeo
-        title={data?.metaTitle}
-        description={data?.metaDescription}
+        title={serviceContent?.metaTitle}
+        description={serviceContent?.metaDescription}
         canonical={`https://aadmirals.com${router?.pathname}`}
       />
-
-      <SideNav />
-      <Floatingbutton />
-      <Header />
-
-      <div className={styles.mainContainer} fluid id="#bookingForm">
-        <Hero
-          Text={""}
-          Title={data.Description1}
-          img={data.Image1}
-          Form={HomeForm}
-        />
-      </div>
-
-      <div className="container mt-3 mb-3">
-        {eventsTransfer.eventsTransfer.map((data, key) => {
-          return (
-            <div className="row" key={key}>
-              <div className="col-12">
-                {<p>{ReactHtmlParser(data.Description2)}</p>}
-                <img src={data.Image2} className="w-100 mb-4 mt-4" alt="" />
-                <img src={data.Image21} className="w-100 mb-4 mt-4" alt="" />
-                <img src={data.Image3} className="w-100 mb-4 mt-4" alt="" />
-                {<p>{ReactHtmlParser(data.Description3)}</p>}
-                <img src={data.Image4} className="w-100 mb-4 mt-4" alt="" />
-                {<p>{ReactHtmlParser(data.Description4)}</p>}
-                <img src={data.Image5} className="w-100 mb-4 mt-4" alt="" />
-                <img src={data.Image51} className="w-100 mb-4 mt-4" alt="" />
-                {<p>{ReactHtmlParser(data.Description5)}</p>}
-              </div>
-            </div>
-          );
-        })}
-      </div>
-      <div className="container services-links mb-4">
-        <h3>Services</h3>
-        <div>
-          {data.dropdown.map((data, key) => {
-            return (
-              <li key={key}>
-                <Link href={data.url} key={key}>
-                  <a>{data.name}</a>
-                </Link>
-              </li>
-            );
-          })}
-        </div>
-      </div>
-      <div className="container">
-        <div className="row">
-          {fleet.map((data, key) => {
-            return (
-              <div className="col-3 carCardContainer" key={key}>
-                <div className="imageCardContainer">
-                  <img
-                    src={data.image}
-                    className="w-100 rounded bordered"
-                    alt=""
-                  />
-                </div>
-                <h6>{data.name}</h6>
-                <p>{data.description && data.description.substring(0, 20)}</p>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-
-      <Testinmonial testimonials={testimonial} />
-
-      <Askquestion faqs={faqs} />
-
-      <br></br>
-      <br></br>
-      <br></br>
-      <Footer />
+      {isError || isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <div style={{ overflow: "hidden" }}>
+            <SideNav />
+            <Floatingbutton />
+            <Header />
+            {error ? (
+              <Alert className="m-0" color="danger">
+                {error}
+              </Alert>
+            ) : null}
+            <Container className={`${styles.mainContainer} p-0`} fluid>
+              <Hero
+                Text={serviceContent?.heroDescription}
+                Title={
+                  serviceContent?.heroTitle ||
+                  "Galveston Cruise Transfer & Shuttle | IAH/Hobby Airport to Glaveston"
+                }
+                img={serviceContent?.heroImage || bg }
+                Form={HomeForm}
+              />
+            </Container>
+            <Home serviceDetail={serviceContent} />
+            <Footer />
+          </div>
+        </>
+      )}
     </>
   );
 }
-export async function getStaticProps({query}) {
-  let res2 = await axios.get(`${URL}/website-content/events-transfer`);
-  //
-  let data2 = res2.data.modifiedResponse;
-  let data1 = {
-    loading: false,
-    error: null,
-    eventsTransfer: data2,
-  };
-  return {props: {data1}};
+export async function getStaticProps({ query }) {
+  const queryClient = new QueryClient()
+  await queryClient.prefetchQuery(['service-page', "640c6a8e4719540d93c3d76d"], getServiceContentById("640c6a8e4719540d93c3d76d"))
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  }
 }
 export default Events;
